50b2fa86779ddc517292967a205b45e980eae36b
Fares <119789181+FaresYigeremu@users.noreply.github.com>
2024-10-03 22:31:45 +0300

    1st try

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal's triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize the first row
+
+    for i in range(1, n):
+        row = [1]  # First element is always 1
+        # Calculate middle elements
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Last element is always 1
+        triangle.append(row)
+
+    return triangle
