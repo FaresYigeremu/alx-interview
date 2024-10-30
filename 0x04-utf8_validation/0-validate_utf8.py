f3e04a90d22e4d21617beedd4b7ea3fc39e9622b
Fares <faresyigeremu@gmail.com>
2024-10-30 13:44:25 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a method that determines if a set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """
+    Determine if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data: List of integers representing the data bytes.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF  # Only consider the least significant 8 bits
+
+        if num_bytes == 0:
+            # Determine how many bytes the current UTF-8 character has
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
+        else:
+            # Check that the byte is a continuation byte
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
